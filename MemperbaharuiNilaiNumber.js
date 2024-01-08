6d21ff71b1cb834ec0286e44b73db0f491695fd4
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-08 19:26:30 +0800

    Memperbarui Nilai Number Di Dalam Variabel

---
+++ MemperbaharuiNilaiNumber.js
@@ -0,0 +1,19 @@
+let angka = 10;
+
+console.log("Nilai awal angka:", angka); // Output: 10
+
+// Memperbarui nilai variabel angka
+angka = angka + 5;
+console.log("Setelah ditambah 5:", angka); // Output: 15
+
+// Operasi lain untuk memperbarui nilai variabel angka
+angka *= 2;
+console.log("Setelah dikalikan 2:", angka); // Output: 30
+
+// Mengurangi nilai variabel angka
+angka -= 10;
+console.log("Setelah dikurangi 10:", angka); // Output: 20
+
+// Pembagian untuk memperbarui nilai variabel angka
+angka /= 4;
+console.log("Setelah dibagi 4:", angka); // Output: 5
