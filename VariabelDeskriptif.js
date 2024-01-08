e1436db2aa0f0c39e0e7d9c40f59467e8f71928b
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-08 19:33:14 +0800

    Tips Memberikan Nama Variabel Seperti Anak Sendiri

---
+++ VariabelDeskriptif.js
@@ -0,0 +1,24 @@
+// Umur anak-anak
+let umurAnakPertama = 8;
+let umurAnakKedua = 5;
+let umurAnakKetiga = 10;
+
+// Nama anak-anak
+let namaAnakPertama = "Sarah";
+let namaAnakKedua = "Ethan";
+let namaAnakKetiga = "Olivia";
+
+// Status kesehatan anak-anak
+let anakPertamaSehat = true;
+let anakKeduaSehat = false;
+let anakKetigaSehat = true;
+
+// Daftar hobi anak-anak
+let hobiAnakPertama = ["berenang", "membaca"];
+let hobiAnakKedua = ["bermain bola", "menulis cerita"];
+let hobiAnakKetiga = ["menggambar", "menonton film"];
+
+// Nilai rata-rata pelajaran anak-anak
+let nilaiRataRataAnakPertama = 85.5;
+let nilaiRataRataAnakKedua = 70.2;
+let nilaiRataRataAnakKetiga = 92.0;
