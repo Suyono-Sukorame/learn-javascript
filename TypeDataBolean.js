46763487911b446c90d25eac1a4d83cc03b1bdfe
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-08 19:30:01 +0800

    Tipe Data Booleani

---
+++ TypeDataBolean.js
@@ -0,0 +1,16 @@
+let benar = true;
+let salah = false;
+
+console.log(benar); // Output: true
+console.log(salah); // Output: false
+
+// Contoh penggunaan dalam struktur kendali
+let usia = 18;
+let sudahDewasa = usia >= 18; // Hasil evaluasi akan berupa true atau false
+
+if (sudahDewasa) {
+  console.log("Anda sudah dewasa");
+} else {
+  console.log("Anda masih di bawah umur");
+}
+// Output (bergantung pada nilai variabel usia): "Anda sudah dewasa" atau "Anda masih di bawah umur"
