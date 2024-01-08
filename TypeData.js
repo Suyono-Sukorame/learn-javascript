2b16ea35fe0bd83dba677c96944f6bd84df6f5ff
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-08 17:56:23 +0800

    Tipe Data Primitif Dan Developer Tools Console

---
+++ TypeData.js
@@ -0,0 +1,23 @@
+// String
+let nama = "John Doe";
+console.log(typeof nama); // Output: string
+
+// Number
+let umur = 25;
+console.log(typeof umur); // Output: number
+
+// Boolean
+let isActive = true;
+console.log(typeof isActive); // Output: boolean
+
+// Null
+let dataNull = null;
+console.log(typeof dataNull); // Output: object (a known bug in JavaScript)
+
+// Undefined
+let dataUndefined;
+console.log(typeof dataUndefined); // Output: undefined
+
+// Symbol
+const simbol = Symbol("deskripsi");
+console.log(typeof simbol); // Output: symbol
