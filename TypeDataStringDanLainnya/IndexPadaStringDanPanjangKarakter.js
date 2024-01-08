fed011324b0a4310c39493f274ae7ac626cf8ef3
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-08 19:47:56 +0800

    Penjelasan Index Pada String Dan Panjang Karakter

---
+++ TypeDataStringDanLainnya/IndexPadaStringDanPanjangKarakter.js
@@ -0,0 +1,9 @@
+let kalimat = "Hari ini adalah hari yang cerah";
+
+// Mengakses karakter individual dalam string menggunakan indeks
+console.log(kalimat[0]); // Output: H (karakter pada indeks ke-0)
+console.log(kalimat[5]); // Output: i (karakter pada indeks ke-5)
+console.log(kalimat[15]); // Output: h (karakter pada indeks ke-15)
+
+// Mendapatkan panjang string menggunakan properti length
+console.log(kalimat.length); // Output: 31 (jumlah karakter dalam string "Hari ini adalah hari yang cerah")
